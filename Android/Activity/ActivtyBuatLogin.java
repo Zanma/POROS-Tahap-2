3ad8d47120b6bcb951ba6433918061f2bce46491
Zainuri Mahmud <37464390+Zanma@users.noreply.github.com>
2018-03-21 13:55:09 +0700

    Add files via upload

---
+++ Android/Activity/ActivtyBuatLogin.java
@@ -0,0 +1,13 @@
+package com.example.acerpc.iniactivitybuatlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivtyBuatLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activty_buat_login);
+    }
+}
---
+++ Android/Intent/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.acerpc.belajarintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void pindah(View view) {
+        Intent i=new Intent(this, layout2.class);
+        startActivity(i);
+    }
+}
